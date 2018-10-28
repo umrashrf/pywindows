c33954bc9b52d7f66281ad7a9e0acad46bdfefa0
Umair <umrashrf@gmail.com>
2018-10-27 23:56:07 -0700

    changed Install script to install all wheels from downloads directory

--- Install.bat
+++ Install.bat
@@ -1,3 +1,3 @@
 Python\Python37\python.exe -m pip install -U pip
-Python\Python37\python.exe -m pip install git+https://github.com/umrashrf/pytemplate.git#egg=bot
+Python\Python37\python.exe -m pip install downloads\*.whl
 pause
